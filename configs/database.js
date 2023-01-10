14d8ec83b0623dfa4c0d230172b4135d734637d6
hanmadrin <hanmadrin@gmail.com>
2023-01-10 18:50:27 +0600

    log added

--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,12 @@
 const sequelize = require('sequelize');
 require('dotenv').config();
-module.exports =  new sequelize(
-    process.env.DATABASE_NAME||"xentabtw_mmc_chat", 
-    process.env.DATABASE_USERNAME||"xentabtw_mmc", 
-    process.env.DATABASE_PASSWORD||"xentabtw_mmc", 
+console.log(process.env.DATABASE_NAME);
+console.log(process.env.DATABASE_USERNAME);
+console.log(process.env.DATABASE_PASSWORD);
+module.exports = new sequelize(
+    process.env.DATABASE_NAME || "xentabtw_mmc_chat",
+    process.env.DATABASE_USERNAME || "xentabtw_mmc",
+    process.env.DATABASE_PASSWORD || "xentabtw_mmc",
     {
         host: 'localhost',
         dialect: 'mysql',
