b87e89ead05fdc7149d234cd3415905f5ea2689c
hanmadrin <hanmadrin@gmail.com>
2023-02-20 20:13:08 +0600

    db pass changed

--- configs/database.js
+++ configs/database.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 // console.log(process.env.DATABASE_PASSWORD);
 module.exports = new sequelize(
     process.env.DATABASE_NAME || "xentabtw_mmc_chat",
-    process.env.DATABASE_USERNAME || "xentabtw_mmc",
-    process.env.DATABASE_PASSWORD || "xentabtw_mmc",
+    process.env.DATABASE_USERNAME || "rimo",
+    process.env.DATABASE_PASSWORD || "p@ssword69",
     {
         host: '127.0.0.1',
         dialect: 'mysql',
